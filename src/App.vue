21452949d4f9ad2d2f05063dc78134c85fcecfda
Bruno Lucho Macedo <bmacedo@weg.net>
2021-06-24 18:01:27 -0300

    Adicionado mensagens de erro

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,70 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-block p-mt-6">
+    <div class="p-d-flex p-jc-center"><h1 style="height: 4rem;">＄</h1></div>
+    
+    <div class="p-d-flex p-jc-center p-m-4">
+      <div class="p-d-block">
+        <span class="p-float-label">
+          <InputNumber id="dolar" mode="currency" currency="USD" locale="en-US" v-model="valueDolar"/>
+          <label for="dolar">Quantidade em Reais</label>
+        </span>
+      </div>
+      <div class="p-d-flex p-ml-2">
+        <InlineMessage severity="error" v-show="result && !valueDolar">Insira o valor em dólares</InlineMessage>
+      </div>
+    </div>
+
+    <div class="p-d-flex p-jc-center p-m-4">
+      <span class="p-float-label">
+        <InputNumber id="real" mode="currency" currency="USD" locale="en-US" v-model="cotacao"/>
+        <label for="real">Cotação do dolar</label>
+      </span>
+      <div class="p-d-flex p-ml-2">
+        <InlineMessage severity="error" v-show="result && !cotacao">Insira a cotação</InlineMessage>
+      </div>
+    </div>
+
+    <div class="p-d-flex p-jc-center p-m-4">
+      <div class="p-d-inline p-m-2">
+        <Button label="Clear" @click="clear"/>
+      </div>
+      <div class="p-d-inline p-m-2">
+        <Button label="Calculate" @click="calculate" />
+      </div>
+    </div>    
+
+    <div class="p-d-flex p-jc-center p-m-2">
+      <p v-show="result && (valueDolar && cotacao)">Com {{cotacao}} reais é possível comprar {{result}} dólares a {{cotacao}} cada</p>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      messages: [
+        {severity: 'info', content: 'Dynamic Info Message'},
+        {severity: 'success', content: 'Dynamic Success Message'},
+        {severity: 'warn', content: 'Dynamic Warning Message'}
+      ],
+      valueDolar: null,
+      cotacao: null,
+      result: null,
+    }
+  },
+  methods: {
+    clear() {
+      this.valueDolar = null
+      this.cotacao = null
+      this.result = false
+    },
+    
+    calculate() {
+      this.result = (this.valueDolar / this.cotacao).toFixed(2)
+    }
   }
 }
 </script>
@@ -19,8 +74,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from "primevue/config"
+import Button from "primevue/button"
+import InputNumber from "primevue/inputnumber"
+
+import "primevue/resources/themes/saga-blue/theme.css"
+import "primevue/resources/primevue.min.css"
+import InlineMessage from 'primevue/inlinemessage';
+import "primeflex/primeflex.css"
+
+const app = createApp(App)
+app.use(PrimeVue)
+app.component("Button", Button)
+app.component("InputNumber", InputNumber)
+app.component("InlineMessage", InlineMessage)
+
+app.mount('#app')
+
+
